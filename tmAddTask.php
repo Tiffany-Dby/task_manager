93247515900d4137d2678ba4186c5c0a8a7c9d4f
Samp83 <sami.paris@supdevinci-edu.fr>
2024-11-08 11:42:36 +0100

    insertion taches par formulaire

---
+++ tmAddTask.php
@@ -0,0 +1,24 @@
+<?php require_once('tmDatabase.php'); 
+
+    $tmTitle = "";
+    $tmDescription = "";
+?>
+
+    <form method="post" action="">
+    <label for="tmTitle">Titre de la tâche:</label>
+    <input name="tmTitle" id="tmTitle" type="text">
+    <br>   </br>
+    <label for="tmDescription">Description de la tâche:</label>
+    <input name="tmDescription" id="tmDescription" type="text">
+    <br>   </br>
+    <input type="submit" value="Valider">
+</form>
+<?php
+    if (isset($_POST['tmTitle']) && isset($_POST['tmDescription'])) {
+        $tmTitle = $_POST['tmTitle'];
+        $tmDescription = $_POST['tmDescription'];
+        $user_id = 1;
+        $sql = "INSERT INTO tasks (title, description, user_id) VALUES ('$tmTitle', '$tmDescription', $user_id)";
+        $base->exec($sql);
+        
+    }
